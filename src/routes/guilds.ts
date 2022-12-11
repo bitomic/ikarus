ad113bc04e5fa65fce491a4798700c63358a6253
bitomic <35199700+bitomic@users.noreply.github.com>
2022-12-11 13:45:21 -0700

    feat(routes): know if the bot is in a guild or not

---
+++ src/routes/guilds.ts
@@ -0,0 +1,29 @@
+import { type ApiRequest, type ApiResponse, methods, Route, type RouteOptions } from '@sapphire/plugin-api'
+import { ApplyOptions } from '@sapphire/decorators'
+
+interface RequestParams extends Record<string, string> {
+	channel: string
+	guild: string
+	message: string
+}
+
+@ApplyOptions<RouteOptions>( {
+	enabled: true,
+	route: 'guilds/:guild'
+	} )
+export class UserRoute extends Route {
+	public async [ methods.GET ]( request: ApiRequest, response: ApiResponse ): Promise<void> {
+		const { guild } = request.params as RequestParams
+
+		try {
+			await this.container.client.guilds.fetch( guild )
+			response.json( {
+				exists: true
+			} )
+		} catch {
+			response.json( {
+				exists: false
+			} )
+		}
+	}
+}
