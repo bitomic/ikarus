644f4ada7670439042be13120c6e796dbd436698
bitomic <35199700+bitomic@users.noreply.github.com>
2023-01-15 13:13:08 -0700

    feat: starboard

---
+++ src/listeners/starboard/MessageReactionAdd.ts
@@ -0,0 +1,105 @@
+import { ButtonStyle, ChannelType, ComponentType, Events, type Message, type MessageReaction } from 'discord.js'
+import { Listener, type ListenerOptions } from '@sapphire/framework'
+import type { PartialMessage, TextChannel } from 'discord.js'
+import { ApplyOptions } from '@sapphire/decorators'
+import Colors from '@bitomic/material-colors'
+import { env } from '../../lib'
+import Imgur from 'imgur'
+
+@ApplyOptions<ListenerOptions>( {
+	event: Events.MessageReactionAdd
+} )
+export class UserEvent extends Listener {
+	public async run( reaction: MessageReaction ): Promise<void> {
+		if ( reaction.emoji.name !== '⭐' ) return
+		await reaction.fetch()
+		const { message } = reaction
+		if ( !message.guildId || message.partial ) return
+
+		const models = this.container.stores.get( 'models' )
+		const guildSettings = models.get( 'GuildSettings' )
+		const starboardChannel = await guildSettings.get( message.guildId, 'starboard-channel' )
+		if ( !starboardChannel ) return
+
+		const requiredReactions = parseInt( await guildSettings.get( message.guildId, 'starboard-count' ) ?? '1', 10 )
+		if ( reaction.count < requiredReactions ) return
+
+		const starboardMessages = models.get( 'StarboardMessages' )
+		const isPinned = await starboardMessages.has( message.guildId, message.id )
+
+		if ( isPinned ) {
+			const pin = await starboardMessages.get( message.guildId, message.id )
+			if ( pin ) {
+				const guild = await this.container.client.guilds.fetch( message.guildId )
+				const channel = await guild.channels.fetch( pin.pinChannel )
+					.catch( () => null )
+				if ( channel?.isTextBased() ) {
+					const msg = await channel.messages.fetch( pin.pinMessage )
+						.catch( () => null )
+					if ( msg ) {
+						await msg.edit( {
+							content: `⭐ ${ message.reactions.resolve( '⭐' )?.count ?? '¿?' }`
+						} )
+						return
+					}
+				}
+			}
+		}
+
+		const guild = await this.container.client.guilds.fetch( message.guildId )
+		const channel = await guild.channels.fetch( starboardChannel )
+		if ( channel?.type !== ChannelType.GuildText ) return
+
+		const pin = await this.sendNewMessage( channel, message )
+		if ( !pin ) return
+
+		await starboardMessages.set( {
+			channel: message.channel.isThread() ? message.channel.parentId ?? '' : message.channelId,
+			guild: message.guildId,
+			message: message.id,
+			pinChannel: starboardChannel,
+			pinMessage: pin,
+			thread: message.channel.isThread() ? message.channelId : undefined,
+			user: reaction.message.author?.id ?? ''
+		} )
+	}
+
+	protected async sendNewMessage( channel: TextChannel, message: Message | PartialMessage ): Promise<string | null> {
+		const imgur = new Imgur( {
+			clientId: env.IMGUR_CLIENT_ID,
+			clientSecret: env.IMGUR_CLIENT_SECRET
+		} )
+		const image = await imgur.upload( { image: message.author?.avatarURL( { extension: 'png' } ) ?? '' } )
+		const stars = message.reactions.resolve( '⭐' )?.count
+		const pin = await channel.send( {
+			components: [ {
+				components: [ {
+					label: 'Ir al mensaje',
+					style: ButtonStyle.Link,
+					type: ComponentType.Button,
+					url: message.url
+				} ],
+				type: ComponentType.ActionRow
+			} ],
+			content: `⭐ ${ stars ?? '¿?' }`,
+			embeds: [ {
+				author: {
+					icon_url: image.data.link,
+					name: message.author?.tag ?? ''
+				},
+				color: Colors.yellow.s800,
+				description: message.content ?? '',
+				footer: {
+					text: `${ message.id } • #${ 'name' in message.channel ? message.channel.name : message.channel.id }`
+				},
+				image: {
+					url: message.attachments.at( 0 )?.url ?? ''
+				},
+				timestamp: new Date().toISOString()
+			}, ...message.embeds ]
+		} )
+			.catch( () => null )
+
+		return pin?.id ?? null
+	}
+}
---
+++ src/models/GuildSettings.ts
@@ -0,0 +1,81 @@
+import type { ModelStatic, Model as SequelizeModel } from 'sequelize'
+import type { PieceContext, PieceOptions } from '@sapphire/pieces'
+import { DataTypes } from 'sequelize'
+import type { GuildSettingKey } from '../utils'
+import { Model } from '../framework'
+
+interface IGuildSettings {
+	guild: string
+	setting: GuildSettingKey
+	value: string
+}
+
+interface IGuildSettingsInterface extends SequelizeModel<IGuildSettings, IGuildSettings>, IGuildSettings {
+}
+
+export class GuildSettingsModel extends Model<IGuildSettingsInterface> {
+	public readonly model: ModelStatic<IGuildSettingsInterface>
+
+	public constructor( context: PieceContext, options: PieceOptions ) {
+		super( context, {
+			...options,
+			name: 'GuildSettings'
+		} )
+
+		this.model = this.container.sequelize.define<IGuildSettingsInterface>(
+			'GuildSettings',
+			{
+				guild: {
+					primaryKey: true,
+					type: DataTypes.STRING
+				},
+				setting: {
+					primaryKey: true,
+					type: DataTypes.STRING
+				},
+				value: DataTypes.STRING
+			},
+			{
+				tableName: 'GuildSettings',
+				timestamps: false
+			}
+		)
+	}
+
+	protected key( guild: string, setting: GuildSettingKey ): string {
+		return `guild-settings:${ guild }/${ setting }`
+	}
+
+	public async set( options: IGuildSettings ): Promise<void> {
+		await this.model.upsert( options )
+		const key = this.key( options.guild, options.setting )
+		void this.container.redis.set( key, options.value )
+	}
+
+	public async get( guild: string, setting: GuildSettingKey ): Promise<string | null> {
+		const key = this.key( guild, setting )
+		const cached = await this.container.redis.get( key )
+		if ( cached ) return cached
+
+		const result = await this.model.findOne( { where: { guild, setting } } )
+		if ( result ) {
+			void this.container.redis.set( key, result.value )
+		}
+
+		return result?.value ?? null
+	}
+
+	public async getGuildSettings( guild: string ): Promise<Record<string, string>> {
+		const query = await this.model.findAll( { where: { guild } } )
+		return query.reduce( ( settings, item ) => {
+			settings[ item.setting ] = item.value
+			return settings
+		}, {} as Record<string, string> )
+	}
+}
+
+declare global {
+	interface ModelRegistryEntries {
+		GuildSettings: GuildSettingsModel
+	}
+}
