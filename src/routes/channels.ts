2ed32f7c7dda9a265a5970c54e2678fb88332b16
bitomic <35199700+bitomic@users.noreply.github.com>
2023-01-20 13:28:07 -0700

    feat: channels route

---
+++ src/routes/channels.ts
@@ -0,0 +1,23 @@
+import { type ApiRequest, type ApiResponse, methods, Route, type RouteOptions } from '@sapphire/plugin-api'
+import { ApplyOptions } from '@sapphire/decorators'
+
+interface RequestParams extends Record<string, string> {
+	guild: string
+}
+
+@ApplyOptions<RouteOptions>( {
+	enabled: true,
+	route: 'channels/:guild'
+} )
+export class UserRoute extends Route {
+	public async [ methods.GET ]( request: ApiRequest, response: ApiResponse ): Promise<void> {
+		const { guild } = request.params as RequestParams
+
+		try {
+			const g = await this.container.client.guilds.fetch( guild )
+			response.json( g.channels )
+		} catch {
+			response.json( {} )
+		}
+	}
+}
