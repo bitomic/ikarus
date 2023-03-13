53dc924e3595ae2b542d3922b6086e6d152f9e78
bitomic <35199700+bitomic@users.noreply.github.com>
2023-03-13 16:29:45 -0600

    fix: allow empty string for redis, and remove database env

--- src/lib/environment.ts
+++ src/lib/environment.ts
@@ -10,23 +10,21 @@ export const env = load( {
 	DISCORD_TOKEN: String,
 	IMGUR_CLIENT_ID: String,
 	IMGUR_CLIENT_SECRET: String,
-	MYSQL_DATABASE: String,
-	MYSQL_HOST: String,
-	MYSQL_PASSWORD: String,
-	MYSQL_PORT: {
-		default: 3306,
-		type: Number
-	},
-	MYSQL_USERNAME: String,
 	NODE_ENV: [
 		'development' as const,
 		'production' as const
 	],
 	REDIS_DB: Number,
 	REDIS_HOST: String,
-	REDIS_PASSWORD: String,
+	REDIS_PASSWORD: {
+		default: '',
+		type: String
+	},
 	REDIS_PORT: Number,
-	REDIS_USERNAME: String,
+	REDIS_USERNAME: {
+		default: '',
+		type: String
+	},
 	TWITCH_CLIENT: String,
 	TWITCH_SECRET: String
 } )
