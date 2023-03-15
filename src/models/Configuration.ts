ef861d4b92633c6aedefbfb853a50606bc363fe6
bitomic <35199700+bitomic@users.noreply.github.com>
2023-03-14 21:38:47 -0600

    fix: cache configuration settings

--- src/models/Configuration.ts
+++ src/models/Configuration.ts
@@ -11,12 +11,7 @@ export class ConfigurationModel extends Model {
 	public get( guild: string, property: ConfigurationKey, validator?: undefined ): Promise<string | null>
 	public get<T>( guild: string, property: ConfigurationKey, validator: BaseValidator<T> ): Promise<T | null>
 	public async get<T>( guild: string, property: ConfigurationKey, validator?: BaseValidator<T> ): Promise<T | string | null> {
-		const result = ( await this.container.prisma.configuration.findUnique( {
-			select: { value: true },
-			where: {
-				guild_property: { guild, property }
-			}
-		} ) )?.value
+		const result = await this.getFromCache( guild, property )
 		if ( !result ) return null
 		if ( validator ) {
 			const value = validator.run( result )
@@ -24,6 +19,29 @@ export class ConfigurationModel extends Model {
 			return value.unwrap()
 		}
 		return result
+	}
+
+	protected async getFromCache( guild: string, property: ConfigurationKey ): Promise<string | null> {
+		const key = `configuration:${ guild }/${ property }`
+		const cached = await this.container.redis.get( key )
+		if ( cached ) return cached
+
+		const stored = await this.getFromDatabase( guild, property )
+		if ( !stored ) return null
+
+		await this.container.redis.set( key, stored )
+		return stored
+	}
+
+	protected async getFromDatabase( guild: string, property: ConfigurationKey ): Promise<string | null> {
+		const result = await this.container.prisma.configuration.findUnique( {
+			select: { value: true },
+			where: {
+				guild_property: { guild, property }
+			}
+		} )
+
+		return result?.value ?? null
 	}
 }
 
